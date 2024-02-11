8a3ff71b2a540fe010ab3d0128286775202ee052
NiketBhaskar <kniketofficial@gmail.com>
2024-02-12 02:40:11 +0530

    Working basic model issue - api used is forbidden in india key removed

--- app/src/main/java/com/revia/musicapp/api/APIInterface.kt
+++ app/src/main/java/com/revia/musicapp/api/APIInterface.kt
@@ -8,7 +8,6 @@ import retrofit2.http.Query
 
 interface APIInterface {
 
-    @Headers("X-RapidAPI-Key: 1c047ae58emshfa560c3dac8ea3bp18499ajsne9360e0d1274" + "X-RapidAPI-Host: deezerdevs-deezer.p.rapidapi.com")
     @GET("search")
     fun getData(@Query("q") query: String) : Call<MyData>
 }
